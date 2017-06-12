f5e750aed62dded39844be362c7731e902713683
Charles Jackson <nightbeacons@gmail.com>
2017-06-12 01:46:35 +0000

    Initlal commit

---
+++ common.php
@@ -0,0 +1,6 @@
+<?php
+
+$headingNames=array("Name", "CAPID", "Rank", "Joined", "Renew", "Address", "Phone", "Email", "Comments");
+$headingCols=array("name",  "capid", "rank", "joined", "renew", "zip", "phone1", "email", "comments");
+
+?>
